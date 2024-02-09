8185b9819573606add207be921f6d1e745084dfb
Joe5445 <youssef5445@gmail.com>
2024-02-09 18:38:43 +0200

    compse message done eith reply and reply all

--- inbox-app/src/main/java/io/javabrains/inbox/controllers/ComposeController.java
+++ inbox-app/src/main/java/io/javabrains/inbox/controllers/ComposeController.java
@@ -11,8 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class ComposeController {
     @Autowired
@@ -24,6 +29,7 @@ public class ComposeController {
 
     @GetMapping(value = "/compose")
     public String getComposePage(
+            @RequestParam(required = false) String to,
             @AuthenticationPrincipal OAuth2User principal,
             Model model
     ){
@@ -36,6 +42,19 @@ public class ComposeController {
         model.addAttribute("userFolders", userFolders);
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders", defaultFolders);
+        if(StringUtils.hasText(to)){
+
+            String[] splitIds = to.split(",");
+            List<String> uniqueToIds = Arrays.asList(splitIds).
+                    stream()
+                    .map(id -> StringUtils.trimWhitespace(id))
+                    .filter(id -> StringUtils.hasText(id))
+                    .distinct()
+                    .collect(Collectors.toList());
+
+            model.addAttribute("toIds",String.join(", ",uniqueToIds));
+
+        }
 
         return "compose-page" ;
     }
